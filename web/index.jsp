d1df35a79edb142777509553c2caaa2c85b9f588
TaumuLu <972409545@qq.com>
2016-02-23 13:09:45 +0800

    jquery json

--- web/index.jsp
+++ web/index.jsp
@@ -1,58 +1,71 @@
- <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<!-- <%@ page contentType="text/html;charset=UTF-8" language="java" %> -->
+
 <html>
 <head>
-	<meta http-equiv="content-Type" content="text/html; charset=utf-8"/>
-    <title>test</title>
+    <meta http-equiv="content-Type" content="text/html; charset=utf-8"/>
+    <title>Ajax</title>
     <link rel="stylesheet" type="text/css" href="resource/css/mystyle1.css">
+
 </head>
 <body>
+    <!-- 登录注册 -->
+    <div id="header">
 
-	<div id="header">
+        <div class="sign">
+            <div>
+                <a href="#" id="signin">登录</a>
+                <a href="#" id="signup">注册</a>
+            </div>
+        </div>
+        <div id="inbox">
+            <span id="intext">登录</span>
+            <a id="close"></a>
+            <form>
+                <span></span>
+                <input type="text" placeholder="手机/邮箱/用户名" id="dluser" class="signin-text" />
+                <input type="password" placeholder="密码" id="dlpass" class="signin-pass" />
+                <input type="checkbox" checked="checked" />
+                <label>下次自动登录</label>
+                <input type="submit" value="登 录" class="signin-submit" id="btn-in" />
+            </form>
+        </div>
 
-		<div id="ull">	
-		    <a id="dl">登录</a>	
-			<a id="zc">注册</a>
-		</div>
-		<div id="box1" class="dlbox">
-			<span id="dra1">登录</span>
-			<a id="close1" class="myclose"></a>
-			<form>
-				<input type="text" placeholder="手机/邮箱/用户名" id="dluser" />
-				
-				<input type="password" placeholder="密码" id="dlpass" />
-				
-				<input type="checkbox" checked="checked" />
-				<label>下次自动登录</label>
+        <div id="upbox">
+        <h2>注册 (Login)</h2>
+            <form>
+                <input type="text" placeholder="用户名" class="signup-text" />
 
-				<input type="submit" value="登&nbsp录" id="subdl" />
-			</form>
-		</div>
+                <input type="text" placeholder="手机号" class="signup-text" />
 
-		<div id="box2" class="dlbox">
-			<span id="dra2">注册</span>
-			<a id="close2" class="myclose"></a>
-			<form>
-				<input type="text" placeholder="用户名" />
+                <input type="password" placeholder="密码" class="signup-password" />
 
-				<input type="text" placeholder="手机号" />
-				
-				<input type="password" placeholder="密码" />
+                <input type="submit" value="注  册" class="signup-submit" id="btn-up" />
+            </form>
+        </div>
 
-				<input type="submit" value="注&nbsp册" id="subzc" />
-			</form>
-		</div>
-
-	</div>
-	<div id="bodyer">
-		<div class="logobox">
-			<label>在线查询系统：</label>
-			<input class="shu" type="text" name="a" placeholder="请输入用户名或ID" />
-			<input class="suo" type="submit" name="b" value="查询" />
-		</div>
-	</div>
-	<div id="footer">
-		<!-- <p>©2016&nbsp233333</p> -->
-	</div>
+    </div>
+    <!-- 查询结果 -->
+    <div id="bodyer">
+        <div class="search">
+            <label>在线查询系统：</label>
+            <input class="search-text" type="text" name="a" placeholder="请输入用户名或ID" />
+            <input class="btn-search" type="submit" name="b" value="查询" />
+        </div>
+        <table class="show-search" id="tshow">
+            <caption><h2>搜寻结果</h2></caption>
+            <tr>
+                <th style="width:60px">序号</th><th>姓名</th><th>手机号码</th>
+            </tr>
+          <!--   <tr>
+                <td colspan="4">
+                    <input type="button" value="点击查询">
+                </td>
+            </tr> -->
+        </table>
+    </div>
+    <!-- 标注 -->
+    <div id="footer">
+    </div>
 
 <script src="resource/jquery-1.11.3.js" type="text/javascript"></script>
 <script src="resource/javascript/myjs1.js" type="text/javascript"></script>
--- web/resource/javascript/myjs1.js
+++ web/resource/javascript/myjs1.js
@@ -1,140 +1,182 @@
-	function addLoadEvent(func){
-		var oldonload=window.onload;
-		if(typeof window.onload!='function'){
-			window.onload=func;
-		}else{
-			window.onload=function(){
-				oldonload();
-				func();
-			}
-		}
-	}
+    function addLoadEvent(func){
+        var oldonload=window.onload;
+        if(typeof window.onload!='function'){
+            window.onload=func;
+        }else{
+            window.onload=function(){
+                oldonload();
+                func();
+            }
+        }
+    }
 
-	addLoadEvent();
+    var signin=document.getElementById("signin");
+    var signup=document.getElementById("signup");
 
-	var dl=document.getElementById("dl");
-	var zc=document.getElementById("zc");
+    var inbox=document.getElementById("inbox");
+    var upbox=document.getElementById("upbox");
 
-	var dra1=document.getElementById("dra1");
-	var dra2=document.getElementById("dra2");
+    var intext=document.getElementById("intext");
 
-	var close1=document.getElementById("close1")
-	var close2=document.getElementById("close2")
-
-	var box1=document.getElementById("box1");
-	var box2=document.getElementById("box2");
+    var close=document.getElementById("close")
 
 
-	dl.onclick=function(){
-		openNew(box1,close1);
-	}
-	zc.onclick=function(){
-		openNew(box2,close2);
-	}
-	function openNew(box,close){
-	    
-	    box.style.display="block";
-	    //获取页面的高度和宽度
-	    var sWidth=document.documentElement.scrollWidth;
-	    var sHeight=document.documentElement.scrollHeight;  //获取页面的高度
-		
-	    //获取页面的可视区域高度和宽度
-	    var wHeight=document.documentElement.clientHeight;     // 获取页面可视区域的高度
-	    var wWidth=document.documentElement.clientWidth;
-	    
-	    var oMask=document.createElement("div");
-	        oMask.id="mask";
-	        oMask.style.width=sWidth+"px";
-	        oMask.style.height=sHeight+"px";
-	        document.body.appendChild(oMask);    
-	    	
-		//获取登陆框的宽和高
-		var dHeight=box.offsetHeight;   // 获取登陆框的高度
-		var dWidth=box.offsetWidth;      // 获取登陆框的宽度
-		//设置登陆框的left和top
-		box.style.left=wWidth/2-dWidth/2+"px";
-		box.style.top=wHeight/2-dHeight/2+"px";  //设置登陆框top值
-		
-		//点击登陆框以外的区域也可以关闭登陆框
-		oMask.onclick=close.onclick=function(){
-            document.body.removeChild(oMask);
-            box.style.display="none";
-		}
-	}
-	dra1.onmousedown=function(){
-		fnDown(event,box1);
-	}
-	dra2.onmousedown=function(){
-		fnDown(event,box2);
-	}	    
-	function fnDown(event,box){
-	    event = event || window.event;
+    signin.onclick=function(){
+        openNew(inbox,close);
+    }
+    signup.onclick=function(){
+        openNew(upbox);
+    }
+    function openNew(box,close){
+
+        box.style.display="block";
+        //获取页面的高度和宽度
+        var sWidth=document.documentElement.scrollWidth||document.body.scrollWidth;
+        var sHeight=document.documentElement.scrollHeight||document.body.scrollHeight;
+
+        //获取页面的可视区域高度和宽度
+        var wHeight=document.documentElement.clientHeight||document.body.clientHeight;
+        var wWidth=document.documentElement.clientWidth||document.body.clientWidth;
+
+        if(arguments.length==2){
+            var oMask=document.createElement("div");
+                oMask.id="mask";
+                oMask.style.width=sWidth+"px";
+                oMask.style.height=sHeight+"px";
+                document.body.appendChild(oMask);
+
+            //获取登陆框的宽和高
+            var dHeight=box.offsetHeight;
+            var dWidth=box.offsetWidth;
+
+            //设置登陆框的left和top
+            box.style.left=wWidth/2-dWidth/2+"px";
+            box.style.top=wHeight/2-dHeight/2+"px";  /
+
+            //点击登陆框以外的区域也可以关闭登陆框
+            oMask.onclick=close.onclick=function(){
+                document.body.removeChild(oMask);
+                box.style.display="none";
+            }
+        }else{
+            var oMask=document.createElement("div");
+                oMask.id="omask";
+                oMask.style.width=sWidth+"px";
+                oMask.style.height=sHeight+"px";
+            var oclose=document.createElement("div");
+                oclose.id="close";
+                oMask.appendChild(oclose);
+                document.body.appendChild(oMask);
+
+            //获取登陆框的宽和高
+            var dHeight=box.offsetHeight;
+            var dWidth=box.offsetWidth;
+
+            //设置登陆框的left和top
+            box.style.left=wWidth/2-dWidth/2+"px";
+            box.style.top=wHeight/2-dHeight/2+"px";
+
+            //点击登陆框以外的区域也可以关闭登陆框
+            oclose.onclick=function(){
+                document.body.removeChild(oMask);
+                box.style.display="none";
+            }
+        }
+    }
+
+
+    intext.onmousedown=function(){
+        fnDown(event,inbox);
+    }
+    function fnDown(event,box){
+        event = event || window.event;
         // 光标按下时光标和面板之间的距离
         var disX=event.clientX-box.offsetLeft,
             disY=event.clientY-box.offsetTop;
-	    
-	    document.onmousemove=function(event){
-	    	event = event || window.event;
-	    	var a=box;
-	    	fnMove(event,a,disX,disY);
-	    }
-	    // 释放鼠标
-	    document.onmouseup=function(){
-	    	document.onmousemove=null;
-	    	document.onmouseup=null;
-	    }
-	}
 
-	function fnMove(e,a,posX,posY){
-	    var l=e.clientX-posX,
-	        t=e.clientY-posY,
-	        winW=document.documentElement.clientWidth || document.body.clientWidth,
-	        winH=document.documentElement.clientHeight || document.body.clientHeight,
-	        maxW=winW-a.offsetWidth,
-	        maxH=winH-a.offsetHeight;
-	    if(l<0){
-	        l=0;
-	    }else if(l>maxW){
-	        l=maxW;
-	    }
-	    if(t<0){
-	        t=0;
-	    }else if(t>maxH){
-	        t=maxH;
-	    }
-	    a.style.left=l+'px';
-	    a.style.top=t+'px';
-	}
+        document.onmousemove=function(event){
+            event = event || window.event;
+            var a=box;
+            fnMove(event,a,disX,disY);
+        }
+        // 释放鼠标
+        document.onmouseup=function(){
+            document.onmousemove=null;
+            document.onmouseup=null;
+        }
+    }
+    function fnMove(e,a,posX,posY){
+        var l=e.clientX-posX,
+            t=e.clientY-posY,
+            winW=document.documentElement.clientWidth || document.body.clientWidth,
+            winH=document.documentElement.clientHeight || document.body.clientHeight,
+            maxW=winW-a.offsetWidth,
+            maxH=winH-a.offsetHeight;
+        if(l<0){
+            l=0;
+        }else if(l>maxW){
+            l=maxW;
+        }
+        if(t<0){
+            t=0;
+        }else if(t>maxH){
+            t=maxH;
+        }
+        a.style.left=l+'px';
+        a.style.top=t+'px';
+    }
 
 
 $(function(){
-	$("#subdl").click(function(){
+    $("#btn-up").click(function(){
 
-		$.ajax({
-			url: "http://192.168.23.1:8080/login",
+        $.ajax({
+            url: "http://192.168.23.1:8080/register",
 
-			type: "POST",
-			data:{
-				userName:$("#dluser").val(), 
-				passWord:$("#dlpass").val()
-			},
-			dataType: "json",
-			
-			success: function(data){
-				console.log(data.isSuccess);
-				console.log(data.data);
-				if(data.isSuccess){
-					alert("登录成功！");
-					//$("#box1").hide();
-				}else{
-					alert("用户名或密码错误，请重新登录！");
-				}
-			},
-			error: function(){
-				alert("error");
-			},
-		})	
-	})
+            type: "POST",
+            data:{
+                userName:$(".signup-text:eq(0)").val(),
+                passWord:$(".signup-password").val(),
+                age:$(".signup-text:eq(1)").val()
+            },
+            dataType: "json",
+
+            success: function(data){
+                if(data.isSuccess){
+                    alert("注册成功！");
+                }else{
+                    alert("输入错误，请重新填写！");
+                }
+            },
+            error: function(){
+                alert("error");
+            },
+        })
+    })
+    $(".btn-search").click(function(){
+        var url="http://192.168.23.1:8080/list?userName="+$(".search-text").val()
+        $.ajax({
+            url: url,
+
+            type: "GET",
+            dataType: "json",
+
+            success: function(data){
+                if(data.isSuccess){
+                    $.each(data.data,function(index,ele){
+                        $(".show-search").append("<tr><td>"+parseInt(index+1)+"</td><td>"+
+                                            $(ele).attr('name')+"</td><td>"+
+                                            $(ele).attr('age')+"</td></tr>");
+
+                    })
+                }else{
+                    alert("用户名或密码错误，请重新登录！");
+                }
+            },
+            error: function(){
+                alert("error");
+            },
+        })
+    })
 })
-
 
